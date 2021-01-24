4293334bb86641c39491c1f18227deccb10773a7
SaidRem <said.r@yandex.ru>
2021-01-24 10:41:17 +0400

    added func, multy email send

--- email_sc.py
+++ email_sc.py
@@ -1,16 +1,21 @@
 import smtplib
 from email.message import EmailMessage
-from confidential import gmail_address, gmail_password, yandex_address_1
+from confidential import gmail_address, gmail_password, ya_addr_1, ya_addr_2
 
 EMAIL_AD = gmail_address
 EMAIL_PAS = gmail_password
 
-msg = EmailMessage()
-msg['Subject'] = 'This email has been sent through python script'
-msg['From'] = EMAIL_AD
-msg['To'] = EMAIL_AD 
-msg.set_content('Hello friend. My python script actually works')
+def send_email(send_to=None):
+    msg = EmailMessage()
+    msg['Subject'] = 'This email has been sent through python script'
+    msg['From'] = EMAIL_AD
+    msg['To'] = send_to
+    msg.set_content('Hello friend. My python script actually works')
+    
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_AD, EMAIL_PAS)
+        smtp.send_message(msg)
 
-with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-    smtp.login(EMAIL_AD, EMAIL_PAS)
-    smtp.send_message(msg)
+
+if __name__ == '__main__':
+    send_email([gmail_address, ya_addr_2])
