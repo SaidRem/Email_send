608cbd0ae6765784e5af8228f5e7a48a2c0d634b
SaidRem <said.r@yandex.ru>
2021-02-01 17:36:43 +0400

    Added func send() to send through SMTP

--- email_sc.py
+++ email_sc.py
@@ -6,6 +6,13 @@ from confidential import html_content
 EMAIL_AD = gmail_address
 EMAIL_PAS = gmail_pas
 
+
+def send(message):
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_AD, EMAIL_PAS)
+        smtp.send_message(message)
+
+
 def send_email(message=None, send_to=None):
     msg = EmailMessage()
     msg['Subject'] = 'This email has been sent through python script'
@@ -15,10 +22,8 @@ def send_email(message=None, send_to=None):
         msg.set_content(message)
     else:
         msg.set_content('Hello friend. My python script actually works')
-    
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(EMAIL_AD, EMAIL_PAS)
-        smtp.send_message(msg)
+    send(msg)
+
 
 def send_file(filepath=None, message=None, send_to=None):
     if not filepath and send_to:
@@ -34,10 +39,8 @@ def send_file(filepath=None, message=None, send_to=None):
 
     with open(filepath, 'rb') as f:
         msg.add_attachment(f.read(), maintype='application', subtype='octet-stream', filename=f.name)
-    
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(EMAIL_AD, EMAIL_PAS)
-        smtp.send_message(msg)
+    send(msg)
+
 
 def send_html(message=None, send_to=None):
     msg = EmailMessage()
@@ -48,10 +51,10 @@ def send_html(message=None, send_to=None):
         msg.set_content(message, subtype='html')
     else:
         msg.set_content(html_content, subtype='html')
+    send(msg)
+    return "Html sent"
     
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(EMAIL_AD, EMAIL_PAS)
-        smtp.send_message(msg)
+
 
 if __name__ == '__main__':
     # send_email(send_to=[gmail_address, ya_addr_2])
