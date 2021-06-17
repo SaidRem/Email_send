950bc465d8116a62cf7212ea873f579f85b5babb
SaidRem <said.r@yandex.ru>
2021-06-17 23:15:43 +0400

    Added read_messages() func (read messages from your email)

--- read_email.py
+++ read_email.py
@@ -1,5 +1,6 @@
 import imapclient
 import email
+import pprint
 from email.message import EmailMessage
 from confidential import gmail_address, gmail_pas
 
@@ -26,3 +27,23 @@ def unread_email():
         for uid, message_data in imapObj.fetch(messages, "RFC822").items():
             email_message = email.message_from_bytes(message_data[b"RFC822"])
             print(uid, email_message.get('From'), email_message.get('Subject'))
+
+def read_messages():
+    with imapclient.IMAPClient('imap.gmail.com', ssl=True) as imapObj:
+        imapObj.login(EMAIL_AD, EMAIL_PAS)
+        imapObj.select_folder('INBOX', readonly=True)
+        uids = imapObj.search('UNSEEN')
+        raw_messages = imapObj.fetch([uids[-4]], ['BODY[]', 'FLAGS'])  # uids[-3] - 3td from above
+        import pyzmail
+        message = pyzmail.PyzMessage.factory(raw_messages[uids[-4]][b'BODY[]'])
+        
+        print(message.get_subject())
+        print(f'From: {message.get_addresses("from")}')
+        print(f'To: {message.get_addresses("to")}')
+        print(f"cc: {message.get_addresses('cc')}")
+        print(f"bcc: {message.get_addresses('bcc')}")
+        print(f"message.text_part != None => {message.text_part != None}")
+        print(message.text_part.get_payload().decode(message.text_part.charset))
+        print(message.html_part != None)
+        print('='*100)
+        pprint.pprint(message.html_part.get_payload().decode(message.html_part.charset))
